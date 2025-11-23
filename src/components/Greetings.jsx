4c881e1e2b04365139f308bdeae04a7e1bfb0a5e
Utsab Bhusal <utsab2062@gmail.com>
2025-11-23 12:00:32 +0545

    react

---
+++ src/components/Greetings.jsx
@@ -0,0 +1,16 @@
+function Greeting () {
+    return (
+        <div>
+            <h1 className="text-white text-7xl bg-linear-to-r from bg-emerald-400 to via-violet-500 font-bold">
+                Welcome to the Greeting Component !
+            </h1>
+        </div>
+    )
+}
+
+function Hello() {
+    return (
+        <h2 className="text-9xl text-amber-400 bg-linear-to-r bg-emerald-400 via-orange-500">Hello world!</h2>
+    )
+}
+export{Greeting,Hello}
---
+++ src/components/toast.jsx
@@ -0,0 +1,5 @@
+export default function toast() {
+  return (
+    <div>toast</div>
+  )
+}
